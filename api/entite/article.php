f3f9929fe3cbf2b9b4a90805a2a7d4f2d5082c55
Andil Adebiyi <andil.adebiyi@qotto.net>
2020-05-16 14:16:24 +0100

    update

--- api/entite/article.php
+++ api/entite/article.php
@@ -2,23 +2,60 @@
 
 class article 
 {
-    public function get_one_article(){
 
+
+    /**
+     * @author : andil Adebiyi
+     * get_one_article() get specified article from bdd
+     * @param  integer : $id  id for article      
+     * @date : 16/05/2020
+     * @return   array: article with all caractéristque (comment , like,ect) 
+    */
+
+    public function get_one_article($id){
+        $result="";
+        return $result;
     }
 
+
+    /**
+     * @author : andil Adebiyi
+     * get_all_article() get all article  from bdd    
+     * @date : 16/05/2020
+     * @return   array: article with all caractéristque (comment , like,ect) 
+    */
 
     public function get_all_article(){
-
+        $result="";
+        return $result;
     }
 
+
+    /**
+     * @author : andil Adebiyi
+     * save_article() save article in  bdd    
+     * @date : 16/05/2020
+     * @param array : all information for article  
+     * @return   bool: true if is good, false if is not  
+    */
 
     public function save_article(){
-
+        $result="";
+        return $result;
     }
 
 
-    public function del_article(){
+    /**
+     * @author : andil Adebiyi
+     * del_article() del article in  bdd    
+     * @date : 16/05/2020
+     * @param integer : id article for del 
+     * @return   bool: true if is good, false if is not 
+    */
 
+    public function del_article($id){
+        $result="";
+        return $result;
     }
 
 
