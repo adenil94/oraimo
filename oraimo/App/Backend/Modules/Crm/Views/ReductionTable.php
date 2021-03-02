d8587d77a7c1bb89ab792adf0ed77c99a510e702
Kadiri3A <77429868+Kadiri3A@users.noreply.github.com>
2021-03-02 10:31:09 +0100

    Create ReductionTable.php
    Création de la page liste des reductions et un bouton pour l'ajout des réductions

---
+++ oraimo/App/Backend/Modules/Crm/Views/ReductionTable.php
@@ -0,0 +1,70 @@
+
+		<div class="main-panel">
+			<div class="content">
+				<div class="page-inner">
+					<div class="page-header">
+						<h4 class="page-title">réduction</h4>
+						<ul class="breadcrumbs">
+							<li class="nav-home">
+								<a href="#">
+									<i class="flaticon-home"></i>
+								</a>
+							</li>
+							<li class="separator">
+								<i class="flaticon-right-arrow"></i>
+							</li>
+							<li class="nav-item">
+								<a href="#"></a>
+							</li>
+							<li class="separator">
+								<i class="flaticon-right-arrow"></i>
+							</li>
+							<li class="nav-item">
+								<a href="#">Tous les réductions</a>
+							</li>
+						</ul>
+					</div>
+					<div class="page-category">
+						<div class="content content-full">
+							<div class="page-navs bg-white">
+								<div class="nav-scroller">
+									<div class="nav nav-tabs nav-line nav-color-secondary d-flex align-items-center justify-contents-center w-100">
+										<a class="nav-link active show" data-toggle="tab" href="#tab1">Lites des réductions
+											<span class="count ml-1"></span>
+										</a>
+										
+										<div class="ml-auto">
+											<a href="/crm-admin-oraimo/reduction" class="btn btn-success">Ajouter</a>
+										</div>
+									</div>
+								</div>
+								
+
+									<table id="tablereduction" class="display table table-striped table-hover" >
+											<thead>
+												<tr>
+													<th>Nom de l'article</th>
+													<th>reduction</th>
+													<th>date debut</th>
+													<th>date fin</th>
+													<th>action</th>
+													
+												</tr>
+											</thead>
+
+											<tbody id="tablereductionbody">
+												
+											</tbody>
+									</table>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div> 
+
+
+
+<script type="text/javascript">
+	datareduction();
+</script>
